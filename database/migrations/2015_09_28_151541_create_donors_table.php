d6022c01d65197be5af8212f329d49520ca5fbe5
Raj Abishek <rajabishek@hotmail.com>
2015-09-28 22:32:07 +0530

    Migration for the donors table

---
+++ database/migrations/2015_09_28_151541_create_donors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDonorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donors', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('organisation_id')->unsigned();
+            $table->foreign('organisation_id')->references('id')->on('organisations')->onDelete('cascade');
+
+            $table->string('fullname');
+            $table->string('email')->unique();
+            $table->string('mobile')->unique();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('donors');
+    }
+}
