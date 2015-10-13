e17038d35d8926fa9c510cb0f58743fe0d98df20
Raj Abishek <rajabishek@hotmail.com>
2015-10-13 16:38:23 +0530

    Add the routes for the static pages and the account authentication

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,37 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group(['domain' => getenv('APP_DOMAIN')], function()
+{
+	Route::get('/',['as' => 'home','uses' => 'PagesController@getHome']);
+	Route::get('about',['as' => 'about','uses' => 'PagesController@getAbout']);
+	Route::get('pricing',['as' => 'pricing','uses' => 'PagesController@getPricing']);
+	Route::get('support',['as' => 'support','uses' => 'PagesController@getSupport']);
+	Route::get('contact',['as' => 'contact','uses' => 'PagesController@getContact']);
+
+	Route::get('maps','PagesController@getMap');
+		
+
+	Route::get('accounts/register', [ 'as' => 'auth.getRegister', 'uses' => 'AuthController@getRegister' ]);
+	Route::post('accounts/register', ['as' => 'auth.postRegister','uses' => 'AuthController@postRegister']);
+	Route::get('auth/register/verify/{confirmationCode}',['as' => 'auth.register.confirm','uses' => 'AuthController@confirm']);
+
+	Route::get('auth/verification/resend',['as' => 'auth.verification.getResend','uses' => 'AuthController@getResend']);
+	Route::post('auth/verification/resend',['as' => 'auth.verification.postResend','uses' => 'AuthController@postResend']);
+
+});
+
+Route::group(['domain' => '{domain}.' . env('APP_DOMAIN','helpsmile.dev'),'before' => 'verify-domain'], function()
+{
+	Route::get('/',function($domain){ return redirect()->route('auth.getLogin',$domain); });
+
+	//Route::controller('password', 'RemindersController');
+
+	# Authentication and registration routes
+	Route::get('accounts/login', [ 'as' => 'auth.getLogin', 'uses' => 'AuthController@getLogin' ]);
+	Route::post('accounts/login', ['as' => 'auth.postLogin','uses' => 'AuthController@postLogin']);
+
+	Route::get('accounts/logout', [ 'as' => 'auth.logout', 'uses' => 'AuthController@getLogout' ]);
+
 });
