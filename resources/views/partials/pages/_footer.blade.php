fd1b51badd2b364c5f330c4895f900efe17c62d4
Raj Abishek <rajabishek@hotmail.com>
2015-10-13 16:51:30 +0530

    Add the partial for the footer in the static page

---
+++ resources/views/partials/pages/_footer.blade.php
@@ -0,0 +1,72 @@
+<footer id="page-footer" class="bg-white">
+    <div class="content content-boxed">
+        <!-- Footer Navigation -->
+        <div class="row push-30-t items-push-2x">
+            <div class="col-sm-4">
+                <h3 class="h5 font-w600 text-uppercase push-20">Company</h3>
+                <ul class="list list-simple-mini font-s13">
+                    <li>
+                        <a class="font-w600" href="{{ route('home') }}">Home</a>
+                    </li>
+                    <li>
+                        <a class="font-w600" href="frontend_features.html">Features</a>
+                    </li>
+                    <li>
+                        <a class="font-w600" href="frontend_pricing.html">Pricing</a>
+                    </li>
+                    <li>
+                        <a class="font-w600" href="{{ route('about') }}">About Us</a>
+                    </li>
+                    <li>
+                        <a class="font-w600" href="frontend_contact.html">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-sm-4">
+                <h3 class="h5 font-w600 text-uppercase push-20">Support</h3>
+                <ul class="list list-simple-mini font-s13">
+                    <li>
+                        <a class="font-w600" href="{{ route('auth.getLogin') }}">Log In</a>
+                    </li>
+                    <li>
+                        <a class="font-w600" href="{{ route('auth.getLogin') }}">Sign Up</a>
+                    </li>
+                    <li>
+                        <a class="font-w600" href="frontend_support.html">Support Center</a>
+                    </li>
+                    <li>
+                        <a class="font-w600" href="javascript:void(0)">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a class="font-w600" href="javascript:void(0)">Terms &amp; Conditions</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-sm-4">
+                <h3 class="h5 font-w600 text-uppercase push-20">Get In Touch</h3>
+                <div class="font-s13 push">
+                    <strong>Compress, Inc.</strong><br>
+                    340 S LEMON AVE #5612N<br>
+                    LA, CA 91788<br>
+                    <abbr title="Phone">P:</abbr> (123) 456-7890
+                </div>
+                <div class="font-s13">
+                    <i class="si si-envelope-open"></i> info@compress.io
+                </div>
+            </div>
+        </div>
+        <!-- END Footer Navigation -->
+
+        <!-- Copyright Info -->
+        <div class="font-s12 push-20 clearfix">
+            <hr class="remove-margin-t">
+            <div class="pull-right">
+                Crafted with <i class="fa fa-heart text-city"></i> by <a class="font-w600" href="http://compress.io" target="_blank">compress</a>
+            </div>
+            <div class="pull-left">
+                <a class="font-w600" href="javascript:void(0)" target="_blank">Helpsmile 1.0</a> &copy; <span class="js-year-copy"></span>
+            </div>
+        </div>
+        <!-- END Copyright Info -->
+    </div>
+</footer>
