4678388b62744a66a9fe0af4a340f9219b1db661
Raj Abishek <rajabishek@hotmail.com>
2015-10-16 15:03:58 +0530

    Add the abstarct transformer that converts collection to proper format

---
+++ app/Transformers/Transformer.php
@@ -0,0 +1,24 @@
+<?php namespace Helpsmile\Transformers;
+
+abstract class Transformer{
+
+	/**
+     * Transform the given collection of items
+     *
+     * @param array $items
+     * @return array
+     */
+    public function transformCollection(array $items){
+
+        return array_map([$this, 'transform'], $items);
+    }
+
+    /**
+     * Transform the given item
+     *
+     * @param array $item
+     * @return array
+     */
+    public abstract function transform($item);
+	
+}
