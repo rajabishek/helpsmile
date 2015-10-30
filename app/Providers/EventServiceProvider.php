05904d6a0d8e081eb45ece03e0fe60efdd78f353
Raj Abishek <rajabishek@hotmail.com>
2015-10-30 15:17:20 +0530

    Register all the events to its listeners

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -28,6 +28,33 @@ class EventServiceProvider extends ServiceProvider
     {
         parent::boot($events);
 
-        //
+        $this->app['events']->listen('tymon.jwt.expired', function()
+        {
+            return response()->json(['success' => false,'errors' => ['Your token has expired.']], 401);
+        });
+
+        $this->app['events']->listen('tymon.jwt.invalid', function()
+        {
+            return response()->json(['success' => false,'errors' => ['Your token is invalid.']], 400);
+        });
+
+        $this->app['events']->listen('tymon.jwt.absent', function()
+        {
+            return response()->json(['success' => false,'errors' => ['Please provide a token.']],400);
+        });
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $listeners = $this->app['config']->get('helpsmile.listeners');
+
+        foreach ($listeners as $listener) {
+            $this->app['events']->listen('Helpsmile.*',$listener);
+        }
     }
 }
