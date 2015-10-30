d230716ac9375143f6403784a54b89e7b5e75ff5
Raj Abishek <rajabishek@hotmail.com>
2015-10-30 14:57:17 +0530

    Add the view for the manager to track the performance of teamleaders

---
+++ resources/views/manager/employees/teamleaders.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.master')
+
+@section('title', 'Team Leaders')
+
+@section('content')
+<!-- Page Container -->
+<div id="page-container" class="sidebar-l sidebar-o side-scroll header-navbar-fixed">
+    
+    <!-- Sidebar -->
+    @include('partials.manager._sidebar')
+    <!-- END Sidebar -->
+
+    <!-- Header -->
+    @include('partials._header')
+    <!-- END Header -->
+
+    <!-- Main Container -->
+    <main id="main-container">
+        <!-- Page Header -->
+        <div class="content bg-gray-lighter">
+            <div class="row items-push">
+                <div class="col-sm-7">
+                    <h1 class="page-heading">Teamleaders performance</h1>
+                </div>
+                <div class="col-sm-5 text-right hidden-xs">
+                    <ol class="breadcrumb push-10-t">
+                        <li>Manage</li>
+                        <li><a class="link-effect" href="">Teamleaders</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+        <!-- End Page Header -->
+
+        <!-- Page Content -->
+        <div class="content">
+            <!-- User Image Widgets -->
+            <div class="row">
+                @foreach($teamleaders as $teamleader)
+                    <div class="col-xs-12 col-sm-6 col-md-4">
+                        <a class="block block-link-hover3" href="javascript:void(0)">
+                            <div class="block-content block-content-full text-center">
+                                <div>
+                                    <img class="img-avatar img-avatar96" src="{{ $teamleader->photocss }}" alt="">
+                                </div>
+                                <div class="h5 push-15-t push-5">{{ str_limit($teamleader->fullname, 21, $end = '..')}}</div>
+                            </div>
+                            <div class="block-content text-center">
+                                <div class="row items-push">
+                                    <div class="col-xs-6">
+                                        <div class="h3 push-5">{{ $teamleader->total_donations }}</div>
+                                        <div class="h5 font-w300 text-muted">Donations</div>
+                                    </div>
+                                    <div class="col-xs-6">
+                                        <div class="h3 push-5">₹ {{ $teamleader->total_earnings }}</div>
+                                        <div class="h5 font-w300 text-muted">Raised</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+            <!-- END User Image Widgets -->
+        </div>
+        <!-- END Page Content -->
+    </main>
+    <!-- END Main Container -->
+
+    <!-- Footer -->
+    @include('partials._footer')
+    <!-- END Footer -->
+
+</div>
+<!-- END Page Container -->
+@stop
