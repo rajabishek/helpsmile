48d3eb49c252ae26820a5603aaa1056cb6abd2c7
Raj Abishek <rajabishek@hotmail.com>
2016-05-15 21:35:29 +0530

    Adds a minor change for checking

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,7 +16,7 @@ class HttpsProtocol
     public function handle($request, Closure $next)
     {
         $request->setTrustedProxies([$request->getClientIp()]);
-        if (($request->header('x-forwarded-proto') <> 'https') && env('USE_HTTPS', false)) {
+        if (!$request->secure() && env('USE_HTTPS') == true) {
             return redirect()->secure($request->path());
         }
 
