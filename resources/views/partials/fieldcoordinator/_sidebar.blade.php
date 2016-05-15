7f7fd399d544849e4c6b3c83b250b6338aab7b09
Raj Abishek <rajabishek@hotmail.com>
2016-05-15 17:33:04 +0530

    Pass the subdomain to the partial to generate the route

--- resources/views/partials/fieldcoordinator/_sidebar.blade.php
+++ resources/views/partials/fieldcoordinator/_sidebar.blade.php
@@ -10,7 +10,7 @@
                 <button class="btn btn-link text-gray pull-right hidden-md hidden-lg" type="button" data-toggle="layout" data-action="sidebar_close">
                     <i class="fa fa-times"></i>
                 </button>
-                <a class="h5 text-white" href="{{ route('fieldcoordinator.donations.index') }}">
+                <a class="h5 text-white" href="{{ route('fieldcoordinator.donations.index',$domain) }}">
                     <i class="fa fa-circle-o-notch text-primary"></i> <span class="h4 font-w600 sidebar-mini-hide">ne</span>
                 </a>
             </div>
