6f3f6f3201be92792467253c377579b884a5bd89
Raj Abishek <rajabishek@hotmail.com>
2016-05-15 19:27:03 +0530

    Adds the http middleware to redirect all http requests to http

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Helpsmile\Http\Middleware;
+
+use Closure;
+
+class HttpsProtocol
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$request->secure() && env('USE_HTTPS') === true) {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}
